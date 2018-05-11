1079dbfe96b828757a1a92b9777c4936227a9c9e
Kacper Ciepluch <kacper-ciepluch@o2.pl>
2018-05-11 23:03:45 +0200

    create UserFileDataFetcher implementation

--- src/main/java/fetcher/UserFileDataFetcher.java
+++ src/main/java/fetcher/UserFileDataFetcher.java
@@ -1,13 +1,26 @@
 package fetcher;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class UserFileDataFetcher implements FileDataFetcher<User> {
 
     @Override
-    public FileData<User> fetch(Path userDataFilePath, Supplier<User> constructor) {
-        return new FileData<>();
+    public FileData<User> fetch(Path userDataFilePath, String delimiter) throws IOException {
+        try (Stream<String> lines = Files.lines(userDataFilePath)) {
+            return lines
+                    .map(line -> line.split(delimiter))
+                    .filter(this::convertibleToUser)
+                    .map(User::new)
+                    .collect(Collectors.collectingAndThen(Collectors.toSet(), FileData::new));
+        }
     }
+
+    private boolean convertibleToUser(String[] strings) {
+        return strings.length >= 3;
+    }
+
 }
