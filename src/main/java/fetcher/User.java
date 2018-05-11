b8dc844a9d0ae0031c2bb844fa83587afec08290
Kacper Ciepluch <kacper-ciepluch@o2.pl>
2018-05-12 00:13:16 +0200

    create builder for User class

--- src/main/java/fetcher/User.java
+++ src/main/java/fetcher/User.java
@@ -1,7 +1,57 @@
 package fetcher;
 
+import java.time.LocalDate;
+
 class User {
 
-    User(String... userData) {
+    private final String firstName;
+    private final String lastName;
+    private final LocalDate birthday;
+    private final String phoneNumber;
+
+    private User(UserBuilder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.birthday = builder.birthDay;
+        this.phoneNumber = builder.phoneNumber;
+    }
+
+    static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
+    public static class UserBuilder {
+
+        private String firstName;
+        private String lastName;
+        private LocalDate birthDay;
+        private String phoneNumber;
+
+        UserBuilder() { }
+
+        UserBuilder firstName(String firstName) {
+            this.firstName = lastName;
+            return this;
+        }
+
+        UserBuilder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        UserBuilder birthday(LocalDate birthDay) {
+            this.birthDay = birthDay;
+            return this;
+        }
+
+        UserBuilder phoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        User build() {
+            return new User(this);
+        }
+
     }
 }
--- src/main/java/fetcher/UserFileDataFetcher.java
+++ src/main/java/fetcher/UserFileDataFetcher.java
@@ -3,24 +3,43 @@ package fetcher;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class UserFileDataFetcher implements FileDataFetcher<User> {
 
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
     @Override
     public FileData<User> fetch(Path userDataFilePath, String delimiter) throws IOException {
         try (Stream<String> lines = Files.lines(userDataFilePath)) {
             return lines
-                    .map(line -> line.split(delimiter))
-                    .filter(this::convertibleToUser)
-                    .map(User::new)
+                    .map(line -> line.trim().split(delimiter))
+                    .filter(this::hasRequiredColumnValues)
+                    .map(this::covertToUser)
                     .collect(Collectors.collectingAndThen(Collectors.toSet(), FileData::new));
         }
     }
 
-    private boolean convertibleToUser(String[] strings) {
-        return strings.length >= 3;
+    private User covertToUser(String[] validLines) {
+
+        String phoneNumber = validLines.length >= 4 ? validLines[3] : null;
+
+        return User.builder()
+                .firstName(validLines[0])
+                .lastName(validLines[1])
+                .birthday(LocalDate.parse(validLines[2], formatter))
+                .phoneNumber(phoneNumber)
+                .build();
     }
+
+    private boolean hasRequiredColumnValues(String[] columns) {
+        return columns.length >= 3;
+    }
+
+
 
 }
