781325043a283c1053c6b1172051b762e74ee2af
Kacper Ciepluch <kacper-ciepluch@o2.pl>
2018-05-12 16:08:15 +0200

    refactor FetcherMain.java

--- src/main/java/fetcher/FetcherMain.java
+++ src/main/java/fetcher/FetcherMain.java
@@ -1,13 +1,15 @@
 package fetcher;
 
+import java.io.IOException;
+
 import fetcher.service.FetcherConfiguration;
 import fetcher.service.FileData;
 import fetcher.service.FileDataFacade;
 import fetcher.service.User;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 import java.util.Collections;
 import java.util.Set;
 
@@ -29,7 +31,7 @@ class FetcherMain {
                 .findMinBy(User::getBirthday)
                 .showData();
 
-        System.out.printf("File contains %d valid users", allUsers);
+        System.out.printf("File contains %d valid users\n", allUsers);
         System.out.println("The oldest user(s) with phone number are:");
         oldestUsersWithPhoneNumber.forEach(System.out::println);
 
