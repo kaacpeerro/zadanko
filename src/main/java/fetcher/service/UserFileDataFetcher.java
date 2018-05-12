466fcf60c7e02d421e31ac7bd68761121d4c0078
Kacper Ciepluch <kacper-ciepluch@o2.pl>
2018-05-12 16:02:12 +0200

    add trim functionality on mapping file data

--- src/main/java/fetcher/service/UserFileDataFetcher.java
+++ src/main/java/fetcher/service/UserFileDataFetcher.java
@@ -1,10 +1,13 @@
 package fetcher.service;
 
 import java.io.IOException;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,17 +25,24 @@ class UserFileDataFetcher implements FileDataFetcher<User> {
     public FileData<User> fetch(Path userDataFilePath, String delimiter) throws IOException {
         try (Stream<String> lines = Files.lines(userDataFilePath)) {
             return lines
-                    .map(line -> line.trim().split(delimiter))
+                    .map(line -> line.split(delimiter))
+                    .map(this::trimArray)
                     .filter(fileDataValidator::isValid)
                     .map(this::covertToUser)
                     .collect(Collectors.collectingAndThen(Collectors.toSet(), FileData::new));
         }
     }
 
+    private String[] trimArray(String[] strings) {
+        return Stream.of(strings)
+                .map(String::trim)
+                .toArray(String[]::new);
+    }
+
     private User covertToUser(String[] validLines) {
 
         String phoneNumber = validLines.length >= 4 ? validLines[3] : null;
-        
+
         return User.builder()
                 .firstName(validLines[0])
                 .lastName(validLines[1])
