152176e7e74fdad4be20566902e06a2fdf215fe7
Kacper Ciepluch <kacper-ciepluch@o2.pl>
2018-05-12 15:30:48 +0200

    create FetcherMain.java

---
+++ src/main/java/fetcher/FetcherMain.java
@@ -0,0 +1,41 @@
+package fetcher;
+
+import fetcher.service.FetcherConfiguration;
+import fetcher.service.FileData;
+import fetcher.service.FileDataFacade;
+import fetcher.service.User;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Set;
+
+class FetcherMain {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1)
+            return;
+
+        Path filePath = Paths.get(args[0]);
+
+        FileDataFacade fileDataFacade = FetcherConfiguration.fileDataFacade();
+        FileData<User> fileData = fileDataFacade.fetch(filePath, ",");
+
+        int allUsers = fileData.countAll();
+
+        Set<User> oldestUsersWithPhoneNumber = fileData
+                .filterBy(Collections.singleton(FetcherMain::hasPhoneNumber))
+                .findMinBy(User::getBirthday)
+                .showData();
+
+        System.out.printf("File contains %d valid users", allUsers);
+        System.out.println("The oldest user(s) with phone number are:");
+        oldestUsersWithPhoneNumber.forEach(System.out::println);
+
+    }
+
+    private static boolean hasPhoneNumber(User user) {
+        return user.getPhoneNumber() != null;
+    }
+}
